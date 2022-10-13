73d8b884d5bac01bd34167c4f588e5b6c900ae3c
Zac Poorman <33203487+OOCAZ@users.noreply.github.com>
2022-10-13 16:29:56 -0600

    added the working timer and a couple other things

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,39 +1,101 @@
+import { Button, Collapse, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import React, { useRef } from 'react';
+import Countdown, { CountdownApi } from 'react-countdown';
+import { MdLockOutline } from 'react-icons/md';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
 import './App.css';
 
-const Hello = () => {
+const MainPage = () => {
+  const [minuteValue, setMinuteValue] = React.useState(false);
+  const [minuteNumber, setMinuteNumber] = React.useState(10000);
+  const countDownRef = useRef();
   return (
     <div>
       <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
+        <MdLockOutline size="10em" />
       </div>
-      <h1>electron-react-boilerplate</h1>
+      <Typography variant="h2" sx={{ textAlign: 'center' }}>
+        LocTight for Educators
+      </Typography>
+      <Collapse in={minuteValue} sx={{ alignItems: 'center' }}>
+        <Typography variant="h4" sx={{ textAlign: 'center' }}>
+          Time Remaining:
+          <Box>
+            <Countdown
+              date={Date.now() + minuteNumber}
+              autoStart={false}
+              ref={countDownRef}
+              onComplete={() => {
+                setMinuteValue(false);
+              }}
+            />
+          </Box>
+        </Typography>
+        <Button
+          variant="contained"
+          size="large"
+          sx={{ m: 2 }}
+          color="success"
+          onClick={() => {
+            countDownRef.current.api.start();
+          }}
+        >
+          <Typography>Start Count Down</Typography>
+        </Button>
+        <Button
+          variant="contained"
+          size="large"
+          sx={{ m: 2 }}
+          color="warning"
+          onClick={() => {
+            countDownRef.current.api.pause();
+            // setMinuteValue(false);
+          }}
+        >
+          <Typography>Pause Count Down</Typography>
+        </Button>
+        <Button
+          variant="contained"
+          size="large"
+          sx={{ m: 2 }}
+          color="error"
+          onClick={() => {
+            countDownRef.current.api.stop();
+          }}
+        >
+          <Typography>Reset Count Down</Typography>
+        </Button>
+      </Collapse>
       <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
+        <Button
+          variant="contained"
+          size="large"
+          sx={{ m: 2 }}
+          onClick={() => {
+            setMinuteValue(true);
+            setMinuteNumber(5400000);
+          }}
         >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
+          <span role="img" aria-label="books">
+            📚
+          </span>
+          <Typography>90 minute class</Typography>
+        </Button>
+        <Button
+          variant="contained"
+          size="large"
+          sx={{ m: 2 }}
+          onClick={() => {
+            setMinuteValue(true);
+            setMinuteNumber(2700000);
+          }}
         >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+          <span role="img" aria-label="books">
+            📚
+          </span>
+          <Typography>45 minute class</Typography>
+        </Button>
       </div>
     </div>
   );
@@ -43,7 +105,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<MainPage />} />
       </Routes>
     </Router>
   );
